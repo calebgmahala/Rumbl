f0698cea484043b781583289ec942399f48559e8
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-04-13 14:53:50 -0400

    chapter 5-7

---
+++ web/models/video.ex
@@ -0,0 +1,21 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field(:url, :string)
+    field(:title, :string)
+    field(:description, :string)
+    belongs_to(:user, Rumbl.User)
+    belongs_to(:category, Rumbl.Category)
+    timestamps
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ :invalid) do
+    struct
+    |> cast(params, [:url, :title, :description])
+    |> validate_required([:url, :title, :description])
+  end
+end
